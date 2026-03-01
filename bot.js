bf5c2c229754e455d4b3b665f38345586d749951
Edwin Corredor <fuerzag2a@gmail.com>
2026-02-28 23:43:08 -0500

    usar variable de entorno TOKEN

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // 🔴 Pega aquí tu token real
-const token = '8765910526:AAHL9R0DNus0opUJ6AlXhdqFgHTyA4bHStY'
+const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
